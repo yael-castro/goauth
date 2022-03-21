78cf746d8fffe6ac3c2542c64b0109f60310ac1a
Yael Castro <yy.lgnd@gmail.com>
2022-03-20 18:34:27 -0600

    Type included in model package for OAuth errors
    I created the data type "model.OAuthError" based on uint to manage the
    OAuth errors
    
    issue develop

--- internal/model/error.go
+++ internal/model/error.go
@@ -1,17 +1,53 @@
 package model
 
-// ValidationError returns when occurs a validation error
-type ValidationError string
+import "fmt"
 
-func (e ValidationError) Error() string {
-	return string(e)
+// _ "implement" constraint for OAuthError
+var _ error = OAuthError(0)
+
+type OAuthError uint
+
+// Error parsing the integer value to
+func (e OAuthError) Error() string {
+	switch e {
+	case InvalidRequest:
+		return "invalid_request"
+	case AccessDenied:
+		return "access_denied"
+	case UnauthorizedClient:
+		return "unauthorized_client"
+	case UnsupportedResponseType:
+		return "unsupported_response_type"
+	case InvalidScope:
+		return "invalid_scope"
+	case ServerError:
+		return "server_error"
+	case TemporarilyUnavailable:
+		return "temporarily_unavailable"
+	}
+
+	panic(fmt.Sprintf(`value "%d" is not supported`, e))
 }
 
-// NotFound error implementation
-//
-// Returns when occurs an error related to missing resource
-type NotFound string
-
-func (e NotFound) Error() string {
-	return string(e)
-}
+// Supported values for OAuthError
+const (
+	// InvalidRequest the client is not allowed to request an authorization code using this method,
+	// for example if a confidential client attempts to use the implicit grant type
+	InvalidRequest OAuthError = iota
+	// AccessDenied the user or authorization server denied the request
+	AccessDenied
+	// UnauthorizedClient the client is not allowed to request an authorization code using this method,
+	// for example if a confidential client attempts to use the implicit grant type
+	UnauthorizedClient
+	// UnsupportedResponseType the server does not support obtaining an authorization code using this method,
+	// for example if the authorization server never implemented the implicit grant type
+	UnsupportedResponseType
+	// InvalidScope the requested scope is invalid or unknown
+	InvalidScope
+	// ServerError instead of displaying a 500 Internal Server Error page to the user,
+	// the server can redirect with this error code.
+	ServerError
+	// TemporarilyUnavailable if the server is undergoing maintenance, or is otherwise unavailable,
+	// this error code can be returned instead of responding with a 503 Service Unavailable status code
+	TemporarilyUnavailable
+)
