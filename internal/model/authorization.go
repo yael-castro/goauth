6be06af5aac468ad70e45748a4e313c1238498c1
Yael Castro <yy.lgnd@gmail.com>
2022-03-23 00:23:38 -0600

    Draft for the different app scopes
    I made a new data type "model.Scope" derived of uint to manage the
    different app scopes but at the moment is only a draft
    
    issue develop

--- internal/model/authorization.go
+++ internal/model/authorization.go
@@ -1,5 +1,24 @@
 package model
 
+// Supported scopes (permissions)
+const (
+	// Undefined value that indicates "no permissions"
+	Undefined Scope = 0
+	Create    Scope = 1 << iota
+	Read
+	Update
+	Delete
+)
+
+// Scope bit mask that indicates the different scopes (permissions) for the application
+// Deprecated: at the moment is only a draft
+type Scope uint
+
+// Is validates if the scope received as parameter match to s
+func (s Scope) Is(scope Scope) bool {
+	return s&scope == scope
+}
+
 type (
 	// Authorization request of authorization following the protocol OAuth 2.0
 	Authorization struct {
@@ -12,8 +31,10 @@ type (
 		// ResponseType expected response type (code, ...)
 		ResponseType string
 		// State is used by the application to store request-specific data and/or prevent CSRF attacks (Recommended)
-		State               string
-		CodeChallenge       string
+		State         string
+		CodeChallenge string
+		// CodeChallengeMethod is the method that the token endpoint (authorization endpoint) MUST use to verify
+		// the "code_verifier"
 		CodeChallengeMethod string
 		// RedirectUri is not required by the spec, but your service should require it.
 		// This URL must match one of the URLs the developer registered when creating the application,
@@ -22,7 +43,7 @@ type (
 	}
 
 	Application struct {
-		ClientId     string
+		ClientId     stringkkk
 		ClientSecret string
 		// AllowedOrigins valid redirect URIs
 		AllowedOrigins []string
