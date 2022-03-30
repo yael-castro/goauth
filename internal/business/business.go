a1a5a4d22a94281c198fbc274f40170e99408c93
Yael Castro <yy.lgnd@gmail.com>
2022-03-29 20:45:40 -0600

    Authentication Flows: Authorization Code Grant and PKCE
    I made a draft of the flows "Authorization Code Grant" and "PKCE"
    
    I modified the implementations of "business.Authorizer" for each flow:
    
    - To the PKCE flow I modified the struct "business.ProofKeyCodeExchange"
    
    - To the "Authorization Code Grant" flow I modified the struct "business.AuthorizationCodeGrant"
    
    TODO:
    - Validate the code challenge in the PKCE flow
    
    branch develop

--- internal/business/business.go
+++ internal/business/business.go
@@ -17,16 +17,15 @@ type OAuth map[string]Authorizer
 
 // Authorize selects an Authorizer based on the flow specified to be executed
 func (o OAuth) Authorize(auth model.Authorization) *url.URL {
-	uri, _ := url.Parse(auth.RedirectUri)
+	q := auth.RedirectURL.Query()
 
 	flow, ok := o[auth.ResponseType]
 	if !ok {
-		q := uri.Query()
-
 		q.Set("error", model.UnsupportedResponseType.Error())
 
-		uri.RawQuery = q.Encode()
-		return uri
+		auth.RedirectURL.RawQuery = q.Encode()
+
+		return auth.RedirectURL
 	}
 
 	return flow.Authorize(auth)
--- internal/business/pkce.go
+++ internal/business/pkce.go
@@ -1,15 +1,118 @@
 package business
 
 import (
-	"net/url"
-
 	"github.com/yael-castro/godi/internal/model"
+	"github.com/yael-castro/godi/internal/repository"
+	"net/url"
+	"regexp"
 )
+
+// _ "implement" constraint for ProofKeyCodeExchange
+var _ Authorizer = (*AuthorizationCodeGrant)(nil)
+
+// AuthorizationCodeGrant authorize the allowed redirect URLs
+type AuthorizationCodeGrant struct {
+	finder repository.ApplicationFinder
+}
+
+// SetFinder initializes the instance of repository.ApplicationFinder used to get the model.Application
+func (c *AuthorizationCodeGrant) SetFinder(finder repository.ApplicationFinder) {
+	c.finder = finder
+}
+
+// Authorize validate the application (model.Application) obtained with the received data (model.Authorization)
+//
+// In resume...
+//
+// 1. Identifies the app using the client id and client secret
+//
+// 2. Validates the received state
+//
+// 3. Validates the redirect uri
+func (c AuthorizationCodeGrant) Authorize(auth model.Authorization) *url.URL {
+	q := auth.RedirectURL.Query()
+
+	app, err := c.finder.FindApplication(auth.ClientId)
+	// Obtaining the application data by client id
+	if _, ok := err.(model.NotFound); ok {
+		q.Set("error", model.UnauthorizedClient.Error())
+		q.Set("error_description", err.Error())
+		goto end
+	}
+
+	// Handling internal server errors
+	if err != nil {
+		q.Set("error", model.ServerError.Error())
+		q.Set("error_description", err.Error())
+		goto end
+	}
+
+	// Validating state
+	if ok, _ := regexp.MatchString(`[\x20-\x7E]`, auth.State); !ok {
+		q.Set("error", model.InvalidRequest.Error())
+		q.Set("error_description", "invalid state")
+		goto end
+	}
+
+	// Identifying application using the client id and client secret
+	if app.ClientId != auth.ClientId || app.ClientSecret != auth.ClientSecret {
+		q.Set("error", model.UnauthorizedClient.Error())
+		q.Set("error_description", "client credentials does not match")
+		goto end
+	}
+
+	// Validation of redirect uri
+	for _, origin := range app.AllowedOrigins {
+		if auth.RedirectURL.RawPath == origin {
+			return auth.RedirectURL
+		}
+	}
+
+	q.Set("error", model.UnauthorizedClient.Error())
+	q.Set("error_description", "invalid redirect uri")
+
+end:
+	auth.RedirectURL.RawQuery = q.Encode()
+	return auth.RedirectURL
+}
+
+// _ "implement" constraint for ProofKeyCodeExchange
+var _ Authorizer = ProofKeyCodeExchange{}
 
 // ProofKeyCodeExchange is the "Authorization Code Grant" flow with the extension "Proof Key for Code Exchange"
 // for the OAuth 2.0 protocol
-type ProofKeyCodeExchange struct{}
+type ProofKeyCodeExchange struct {
+	// Authorizer must be an implementation of Authorize Code Grant Flow
+	Authorizer
+	repository.SessionStorage
+}
 
+// Authorize validates the code_challenge and code_challenge_method
 func (p ProofKeyCodeExchange) Authorize(auth model.Authorization) *url.URL {
-	panic("implement me")
+	if uri := p.Authorizer.Authorize(auth); uri.Query().Get("error") != "" { // Redirect URI validation
+		return uri
+	}
+
+	q := auth.RedirectURL.Query()
+
+	if auth.CodeChallengeMethod == "" {
+		auth.CodeChallengeMethod = "PLAIN"
+	}
+
+	if auth.CodeChallengeMethod != "PLAIN" && auth.CodeChallengeMethod != "S256" {
+		q.Set("error", model.InvalidRequest.Error())
+		q.Set("error_description", "invalid code_challenge_method, must be PLAIN or S256")
+		goto end
+	}
+
+	// TODO validate code challenge
+
+	if err := p.SessionStorage.CreateSession(auth); err != nil {
+		q.Set("error", model.ServerError.Error())
+		q.Set("error_description", err.Error())
+	}
+
+end:
+	auth.RedirectURL.RawQuery = q.Encode()
+	return auth.RedirectURL
 }
