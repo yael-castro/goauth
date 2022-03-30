f8735f63b6fc4eec5c60da1bae8c5d12fe420c26
Yael Castro <yy.lgnd@gmail.com>
2022-03-29 21:22:48 -0600

    Deleted everything related to path routing
    Removed everything related to endpoint routing while
    I decide what to use for that Standard library, My own multiplexor or
    some third party library like gorilla/mux
    
    branch develop

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,19 +2,8 @@
 package handler
 
 import (
-	"encoding/json"
-	"fmt"
-	"mime"
 	"net/http"
-	"path"
-
-	"github.com/yael-castro/godi/internal/model"
 )
-
-// NotFound handles not found requests
-func NotFound(w http.ResponseWriter, r *http.Request) {
-	JSON(w, http.StatusNotFound, model.Map{"message": "not found"})
-}
 
 // MethodNotAllowed handles invalid requests using an illegal method
 func MethodNotAllowed(w http.ResponseWriter, r *http.Request) {
@@ -22,63 +11,19 @@ func MethodNotAllowed(w http.ResponseWriter, r *http.Request) {
 	w.Write(nil)
 }
 
-// Healthcheck handles requests made to check the status server
-func Healthcheck(w http.ResponseWriter, r *http.Request) {
+// HealthCheck handles requests made to check the status server
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		MethodNotAllowed(w, r)
 		return
 	}
 
-	JSON(w, http.StatusOK, model.Map{"message": "ok"})
+	w.WriteHeader(http.StatusOK)
+	w.Write(nil)
 }
 
-// JSON function used to send serialized data in json more easier
-func JSON(w http.ResponseWriter, status int, body interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(body)
-}
-
-func Bind(r *http.Request, i interface{}) error {
-	contentType, _, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
-	if err != nil {
-		return err
-	}
-
-	switch contentType {
-	case "application/json":
-		return json.NewDecoder(r.Body).Decode(i)
-	}
-
-	return fmt.Errorf("mime type not supported '%s'", contentType)
-}
-
-// New constructs an empty Handler
-func New() *Handler {
-	return &Handler{}
-}
-
-// Handler main handler used in the ListeAndServe
-type Handler struct {
-	Authentication http.Handler
-	Authorization  http.Handler
-}
-
-// ServeHTTP decides which http.HandlerFunc use based on the http method
-func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	p := path.Join(r.URL.Path, "/")
-
-	switch p {
-	case "/goauth/v1/authenticate":
-		h.Authentication.ServeHTTP(w, r)
-
-	case "/goauth/v1/authorizate":
-		h.Authorization.ServeHTTP(w, r)
-
-	case "/goauth/v1/healthcheck":
-		Healthcheck(w, r)
-
-	default:
-		NotFound(w, r)
-	}
+// NotFound handles request made to not found source
+func NotFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write(nil)
 }
