b10734d0f5b0177f9775084df481ef9d312898a4
Yael Castro <yy.lgnd@gmail.com>
2022-04-08 00:58:09 -0500

    New data type to represents an IP
    I created a new data type based on array of 4 bytes to represents an
    IP address v4
    
    Also creates a constructor of IP
    
    issue model-ip

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,6 +1,11 @@
 package model
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 type Map = map[string]interface{}
 
@@ -13,4 +18,44 @@ type BinaryJSON struct {
 // MarshalBinary returns the serialized data in json encode of I
 func (b BinaryJSON) MarshalBinary() ([]byte, error) {
 	return json.Marshal(b.I)
+}
+
+// NewIP constructor for IP
+// Parse an IP from string
+func NewIP(str string) (ip IP, err error) {
+	parts := strings.Split(str, ".")
+
+	if len(parts) > 4 {
+		err = fmt.Errorf("invalid number of ip segments")
+		return
+	}
+
+	for i, part := range parts {
+		var segment int64
+
+		segment, err = strconv.ParseInt(part, 10, 8)
+		if err != nil {
+			return
+		}
+
+		ip[i] = byte(segment)
+	}
+
+	return
+}
+
+// _ "implement" constraint for IP
+var _ fmt.Stringer = IP{}
+
+// IP data type for ip address v4
+type IP [4]byte
+
+// String transforms an IP to string
+//
+// Example:
+//     xxx.xxx.xxx.xxx
+func (i IP) String() string {
+	itoa := strconv.Itoa
+
+	return itoa(int(i[0])) + "." + itoa(int(i[1])) + "." + itoa(int(i[2])) + "." + itoa(int(i[3]))
 }
