91bd90bc102c5a294f5ce76933bbaf462d94244a
Yael Castro <yy.lgnd@gmail.com>
2022-04-17 19:32:22 -0500

    Modified DI to supports the new changes
    I modified the Testing and Default profiles of dependency injection to
    supports the differet changes made in the business and handler package
    
    issue code-exchange

--- internal/dependency/dependency.go
+++ internal/dependency/dependency.go
@@ -7,11 +7,9 @@ import (
 	"github.com/yael-castro/godi/internal/handler"
 	"github.com/yael-castro/godi/internal/model"
 	"github.com/yael-castro/godi/internal/repository"
-	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
-	"time"
 )
 
 // Profile defines options of dependency injection
@@ -60,38 +58,51 @@ func testingProfile(i interface{}) error {
 		return fmt.Errorf(`invalid type "%T"`, i)
 	}
 
-	random := rand.New(rand.NewSource(time.Now().Unix()))
+	generator := business.JWTGenerator{}
 
-	authorizer := business.OAuth{
-		"code": business.AuthorizationCodeGrant{
-			CodeGenerator: business.CodeGeneratorFunc(func() model.AuthorizationCode {
-				return model.AuthorizationCode(strconv.Itoa(random.Int()))
-			}),
-			Owner: business.OwnerAuthenticator{
-				Storage: &repository.MockStorage{
-					"contacto@yael-castro.com": model.Owner{
-						Password: "$2a$10$g141w.TTnp5Bm/rLNqRRRevOSFhKBdV5KaJYxEDi9U5R9TgkZbfne", // yael.castro
-					},
-				},
-			},
-			Client: business.ClientAuthenticator{
-				Finder: repository.MockClientFinder{
-					"mobile": model.Client{
-						Id: "mobile",
-						AllowedOrigins: []string{
-							"http://localhost/callback",
-							"http://localhost:8080/callback",
-						},
-					},
-				},
-			},
-			Storage: &repository.MockStorage{},
-			PKCE:    business.ProofKeyCodeExchange{},
-		},
+	err := generator.SetPrivateKey([]byte(`
+-----BEGIN RSA PRIVATE KEY-----
+MIIBOgIBAAJBAKU2625UUvCglXhj9vzzHVJmlWKEAsYlQhK3+EvrVgpRDE7YQm4T
+nYspJsYnVNQdNIdM0XRpSSe5p/pNDrjdx88CAwEAAQJAWpuCBqIMUpdfIgWA4Tzb
+qeNEriDD/LNWRznJ3KkWKNVkhN8IQGItjDoPW5FzIbTplb47lj3Xa5/F7SNhh9l8
+YQIhANOmy+f/cyl+o23omSUFq5amoNGhndC4nRbawLB2vUlnAiEAx9UxxG9DxlN+
+neNDESR/o5XUO4HfPsJWbxlp4Dj1xVkCIFdL6biD5WUNBa10jY32m8JkcdplFamc
+K7bcfTOLliErAiEAtlC75wvcOcVTb5k4RxuVmBnKV8BVfVywnwwAnKFbGYECIGIR
+trcoqHkRGY43kVrARGwxQDv6+MGlWLCQ2m9p/mNv
+-----END RSA PRIVATE KEY-----`))
+	if err != nil {
+		return err
 	}
 
-	mux.HandleFunc("/go-auth/v1/authorization", handler.NewAuthorizationHandler(authorizer))
+	grant := business.AuthorizationCodeGrant{
+		TokenGenerator: generator,
+		ScopeParser:    business.NewScopeParser(),
+		CodeGenerator:  business.GenerateRandomCode,
+		Owner: business.OwnerAuthenticator{
+			Storage: &repository.MockStorage{
+				"contacto@yael-castro.com": model.Owner{
+					Id:       "contacto@yael-castro.com",
+					Password: "$2a$10$g141w.TTnp5Bm/rLNqRRRevOSFhKBdV5KaJYxEDi9U5R9TgkZbfne", // yael.castro
+				},
+			},
+		},
+		Client: business.ClientAuthenticator{
+			Finder: repository.MockClientFinder{
+				"mobile": model.Client{
+					Id: "mobile",
+					AllowedOrigins: []string{
+						"http://localhost/callback",
+						"http://localhost:8080/callback",
+					},
+				},
+			},
+		},
+		CodeStorage:    &repository.MockStorage{},
+		SessionStorage: &repository.MockStorage{},
+		PKCE:           business.ProofKeyCodeExchange{},
+	}
 
+	*mux = *handler.NewServeMux(grant)
 	return nil
 }
 
@@ -116,26 +127,33 @@ func defaultProfile(i interface{}) error {
 		Password: os.Getenv("REDIS_PASSWORD"),
 	}
 
+	generator := business.JWTGenerator{}
+
+	err = generator.SetPrivateKey([]byte(os.Getenv("PRIVATE_RSA_KEY")))
+	if err != nil {
+		return err
+	}
+
 	redisClient, err := repository.NewRedisClient(redisSettings)
 	if err != nil {
 		return err
 	}
 
-	authorizer := business.OAuth{
-		"code": business.AuthorizationCodeGrant{
-			CodeGenerator: business.CodeGeneratorFunc(business.NewUUIDCode),
-			Storage:       repository.StateStorage{Client: redisClient},
-			PKCE:          business.ProofKeyCodeExchange{},
-			Owner: business.OwnerAuthenticator{
-				Storage: repository.OwnerStorage{Client: redisClient},
-			},
-			Client: business.ClientAuthenticator{
-				Finder: repository.ClientFinder{Client: redisClient},
-			},
+	grant := &business.AuthorizationCodeGrant{
+		TokenGenerator: generator,
+		CodeGenerator:  business.CodeGeneratorFunc(business.GenerateUUID),
+		SessionStorage: repository.SessionStorage{Client: redisClient},
+		CodeStorage:    repository.StateStorage{Client: redisClient},
+		PKCE:           business.ProofKeyCodeExchange{},
+		Owner: business.OwnerAuthenticator{
+			Storage: repository.OwnerStorage{Client: redisClient},
 		},
+		Client: business.ClientAuthenticator{
+			Finder: repository.ClientFinder{Client: redisClient},
+		},
+		ScopeParser: business.NewScopeParser(),
 	}
 
-	mux.HandleFunc("/go-auth/v1/authorization", handler.NewAuthorizationHandler(authorizer))
-
+	*mux = *handler.NewServeMux(grant)
 	return nil
 }
