4cf5a1b697d019bb898c57962a7ff5b1bc7d255e
Yael Castro <yy.lgnd@gmail.com>
2022-05-02 23:04:41 -0500

    Hard refactor for unit tests of Authenticator interface
    I made a hard refactor to keep working the unit test for
    the implementations of Authenticator interface
    
    issue generics

--- internal/business/authentication_test.go
+++ internal/business/authentication_test.go
@@ -10,6 +10,16 @@ import (
 	"testing"
 )
 
+type AuthenticationTest[T any] struct {
+	Authenticator[T]
+	cases []AuthenticationTestCase[T]
+}
+
+type AuthenticationTestCase[T any] struct {
+	input       T
+	expectedErr error
+}
+
 // authenticationTestCase common test case for the Authenticator interface
 type authenticationTestCase struct {
 	input       interface{}
@@ -20,71 +30,71 @@ type authenticationTestCase struct {
 // Test the method Authenticate for different implementations of the Authenticator interface
 // The implementations that are tested
 func TestAuthenticator_Authenticate(t *testing.T) {
-	tdt := []struct {
-		authenticator Authenticator
-		tests         []authenticationTestCase
-	}{
-		{
-			authenticator: OwnerAuthenticator{
-				Obtainer: &repository.MockStorage[string, model.Owner]{
-					"contacto@yael-castro.com": model.Owner{
-						Id:       "contacto@yael-castro.com",
-						Password: "$2a$10$g141w.TTnp5Bm/rLNqRRRevOSFhKBdV5KaJYxEDi9U5R9TgkZbfne", // yael.castro
-					},
-				},
-			},
-			tests: []authenticationTestCase{
-				{
-					input: model.Owner{
-						Id:       "contacto@yael-castro.com",
-						Password: "yael.castro",
-					},
-				},
-				{
-					input:       model.Owner{},
-					expectedErr: model.AccessDenied,
-				},
-			},
-		},
-		{
-			authenticator: ClientAuthenticator{
-				Obtainer: repository.ObtainerFunc[string, model.Client](repository.MockStorage[string, model.Client]{
-					"mobile": model.Client{
-						AllowedOrigins: []string{"https://goauth.com"},
-					},
-				}.Obtain),
-			},
-			tests: []authenticationTestCase{
-				{
-					input: model.Application{
-						Id: "mobile",
-						RedirectURL: func() *url.URL {
-							uri, _ := url.Parse("https://goauth.com")
-							return uri
-						}(),
-					},
-				},
-				{
-					input:       model.Application{},
-					expectedErr: model.UnauthorizedClient,
-				},
+	ownerAuthenticator := OwnerAuthenticator{
+		Obtainer: &repository.MockStorage[string, model.Owner]{
+			"contacto@yael-castro.com": model.Owner{
+				Id:       "contacto@yael-castro.com",
+				Password: "$2a$10$g141w.TTnp5Bm/rLNqRRRevOSFhKBdV5KaJYxEDi9U5R9TgkZbfne", // yael.castro
 			},
 		},
 	}
 
-	for _, v := range tdt {
-		authenticator := v.authenticator
-		t.Run(reflect.TypeOf(authenticator).String(), func(t *testing.T) {
-			for i, v := range v.tests {
-				t.Run(strconv.Itoa(i+1), func(t *testing.T) {
-					err := authenticator.Authenticate(v.input)
-					if !errors.Is(err, v.expectedErr) {
-						t.Fatalf(`expected error "%v" but got "%v"`, v.expectedErr, err)
-					}
+	testAuthentication(t, AuthenticationTest[model.Owner]{
+		Authenticator: ownerAuthenticator,
+		cases: []AuthenticationTestCase[model.Owner]{
+			{
+				input: model.Owner{
+					Id:       "contacto@yael-castro.com",
+					Password: "yael.castro",
+				},
+			},
+			{
+				input:       model.Owner{},
+				expectedErr: model.AccessDenied,
+			},
+		},
+	})
 
-					t.Log(err)
-				})
-			}
-		})
+	clientAuthenticator := ClientAuthenticator{
+		Obtainer: repository.ObtainerFunc[string, model.Client](repository.MockStorage[string, model.Client]{
+			"mobile": model.Client{
+				AllowedOrigins: []string{"https://goauth.com"},
+			},
+		}.Obtain),
 	}
+
+	testAuthentication(t, AuthenticationTest[model.Application]{
+		Authenticator: clientAuthenticator,
+		cases: []AuthenticationTestCase[model.Application]{
+			{
+				input: model.Application{
+					Id: "mobile",
+					RedirectURL: func() *url.URL {
+						uri, _ := url.Parse("https://goauth.com")
+						return uri
+					}(),
+				},
+			},
+			{
+				input:       model.Application{},
+				expectedErr: model.UnauthorizedClient,
+			},
+		},
+	})
+}
+
+func testAuthentication[T any](t *testing.T, test AuthenticationTest[T]) {
+	authenticator := test.Authenticator
+	t.Run(reflect.TypeOf(authenticator).String(), func(t *testing.T) {
+		for i, v := range test.cases {
+			t.Run(strconv.Itoa(i+1), func(t *testing.T) {
+				err := authenticator.Authenticate(v.input)
+				if !errors.Is(err, v.expectedErr) {
+					t.Fatalf(`expected error "%v" but got "%v"`, v.expectedErr, err)
+				}
+
+				t.Log(err)
+			})
+		}
+	})
 }
